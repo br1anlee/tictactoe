6ccc3e44c5ca572da33b194ac056491be830a060
br1anlee <joonmolee3@gmail.com>
2022-01-30 14:51:09 -0800

    tic tac toe solution

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -22,46 +22,68 @@ let boardLayout = [
     '','',''
 ]
 
+let tieBoard = []
+
 const players = { one: "X" , two: "O"};
 
 let turns = true // taking turns (true means player 1 goes first)
 
-let finished = false  // if finished = true, a win condition is met and players are not longer allowed to play X or O
 
+// if finished = true a win condition is met
+// players are not longer allowed to play X or O
+let finished = false  
 
 
 // taking turns X/O
-// return to exit the function if "X" and "O" is present
 cells.forEach((cell) => {
     cell.addEventListener('click', () => {
         if (cell.innerText === "X" || cell.innerText === "O" || finished === true) {
-            return   
+            return    //return to exit the function if "X" or "O" is present in a cell
         }
         if (turns === true)  {
             boardLayout[cell.id] = players.one
             turns = false
             cell.innerText = players.one
+            resultUpdate.innerText = "Player 2's turn!"
         } else {
             boardLayout[cell.id] = players.two
             turns = true
             cell.innerText = players.two
+            resultUpdate.innerText = "Player 1's turn!"
         } 
         winCondition()
+        tieCondition()
     })
 })
+
+
+// define that counter is 1
+let counter = 1
+function tieCondition () {
+    // if counter equals to 9 and no win condition is found
+    // then split out It is a tie and end the game
+   if (counter == 9 && finished === false){
+       resultUpdate.innerText = "It is a tie!"
+       finished = true
+    // if the top statement is not true then keep adding
+   } else {
+       counter ++
+   }
+}
+
 
 // Win Conditions
 function winCondition () {
     for (let i = 0; i < winConditions.length; i++) {
         let winPattern = winConditions[i]
-        let v1 = winPattern[0]
-        let v2 = winPattern[1]
-        let v3 = winPattern[2]
+        let leftCell = winPattern[0]
+        let middleCell = winPattern[1]
+        let rightCell = winPattern[2]
         // grabs each rows and check in the boardLayout[i] if those values are present, if they are present run the next if statement
-        if (boardLayout[v1] != "" && boardLayout[v2] != "" && boardLayout[v3] != ""){
-            if (boardLayout[v1] == boardLayout[v2] && boardLayout[v2] == boardLayout[v3] && boardLayout[v1] == boardLayout[v3]) {
+        if (boardLayout[leftCell] != "" && boardLayout[middleCell] != "" && boardLayout[rightCell] != ""){
+            if (boardLayout[leftCell] == boardLayout[middleCell] && boardLayout[middleCell] == boardLayout[rightCell]) { // if all cells have the same value (X or O)
                 finished = true // finishes the game and prevents players from adding more O & X
-                if (boardLayout[v1] == players.one) {
+                if (boardLayout[leftCell] == players.one) {
                     resultUpdate.innerText = "Player 1 has won!"
                 } else {
                     resultUpdate.innerText = "Player 2 has won!"
@@ -72,62 +94,18 @@ function winCondition () {
     }
 
 
-
-
 // Reset Function
 function clearGame () {
     for (let i = 0; i < cells.length; i++) {
         cells[i].innerText = ""
         resultUpdate.innerText = ""
-        playerUpdate.innerText = "It is Player 1's turn"
+        playerUpdate.innerText = "Player 1's turn!"
         finished = false
         boardLayout = [
             '','','',
             '','','',
             '','',''
         ]
+        counter = 1
     }
 }
-
-
-
-
-// Conditions for win (Player 1)
-// function checkCondition() {
-//     if (cells[0] === "X" &&  cells[1] === "X" && cells[2] === "X"){
-//         resultUpdate.innerText = "Player 1 has won!"
-//     } else if (cells[3] === "X" && cells[4] === "X" && cells[5] === "X"){
-//         resultUpdate.innerText = "Player 1 has won!"
-//     } else if (cells[6] === "X" && cells[7] === "X" && cells[8] === "X"){
-//         resultUpdate.innerText = "Player 1 has won!"
-//     } else if (cells[0] === "X" && cells[3] === "X" && cells[6] === "X"){
-//         resultUpdate.innerText = "Player 1 has won!"
-//     } else if (cells[1] === "X" && cells[4] === "X" && cells[7] === "X"){
-//         resultUpdate.innerText = "Player 1 has won!"
-//     } else if (cells[2] === "X" && cells[5] === "X" && cells[8] === "X"){
-//         resultUpdate.innerText = "Player 1 has won!"
-//     } else if (cells[0] === "X" && cells[4] === "X" && cells[8] === "X"){
-//         resultUpdate.innerText = "Player 1 has won!"
-//     } else if (cells[0] === "O" && cells[1] === "O" && cells[2] === "O"){
-//         resultUpdate.innerText = "Player 2 has won!"
-//     } else if (cells[3] === "O" && cells[4] === "O" && cells[5] === "O"){
-//         resultUpdate.innerText = "Player 2 has won!"
-//     } else if (cells[6] === "O" && cells[7] === "O" && cells[8] === "O"){
-//         resultUpdate.innerText = "Player 2 has won!"
-//     } else if (cells[0] === "O" && cells[3] === "O" && cells[6] === "O"){
-//         resultUpdate.innerText = "Player 2 has won!"
-//     } else if (cells[1] === "O" && cells[4] === "O" && cells[7] === "O"){
-//         resultUpdate.innerText = "Player 2 has won!"
-//     } else if (cells[2] === "O" && cells[5] === "O" && cells[8] === "O"){
-//         resultUpdate.innerText = "Player 2 has won!"
-//     } else if (cells[0] === "O" && cells[4] === "O" && cells[8] === "O"){
-//         resultUpdate.innerText = "Player 2 has won!"
-//     } else {
-//         resultUpdate.innerText = "It is a tie!"
-//     }
-
-
-
-
-
-// // Track any clicks that happen on the cell & check if a valid move has been made (nothing happens if cell has been played already)
